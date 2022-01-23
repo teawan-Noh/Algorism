6e7d3275086d7d38018c6330bf362c68b5b7f209
DESKTOP-QA0M42J\taewan <ntw2100@gmail.com>
2022-01-23 11:27:43 +0900

    sort

--- src/sort/Select.java
+++ src/sort/Select.java
@@ -4,14 +4,12 @@ public class Select {
 
 	public static void main(String[] args) {
 		
-		int[] arr = {40, 70, 60, 30, 10, 50,15,12,13,44,11,69};
+		int[] arr = {40, 70, 60, 30, 10};
 		System.out.println(arr.length);
 		for (int i = 0; i < arr.length - 1; i++) {
 			int min = arr[i];
 			int tmp= i;
 			for (int j = i+1; j < arr.length ; j++) {
-				//arr[i] 가 시작점 
-				//시작점하고 체인지 
 				if ( min > arr[j]) {
 					min = arr[j];
 					tmp = j;
