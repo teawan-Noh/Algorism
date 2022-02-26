adbe8081ced2cb3573ba5b3b522d166f53602aac
DESKTOP-QA0M42J\taewan <ntw2100@gmail.com>
2022-02-26 16:32:02 +0900

    무브 제로 수정

--- src/practice/MoveZero.java
+++ src/practice/MoveZero.java
@@ -5,19 +5,44 @@ import java.util.ArrayList;
 public class MoveZero {
 
 	public static void main(String[] args) {
-		int[] arr = {0, 8, 0, 37, 4, 5, 0, 50, 0 ,34, 0 ,0};
+		int[] arr = {0, 8, 0, 37, 4, 5, 0, 50, 0, 34, 0, 0, 72};
+//		int[] arr = {0, 5, 0, 3, 0, 0, 7};
 		
+		int idx = 0;
+		int arrLen = arr.length;
+		int cnt = 0;
 		ArrayList<Integer> arrList = new ArrayList<>();
-		for (int i = 0; i < arr.length; i++) {
+		for (int i = 0; i < arrLen; i++) {
 			arrList.add(arr[i]);
 		}
 		
-		for (int i = 0; i < arrList.size(); i++) {
-			if(arrList.get(i) == 0) {
-				arrList.remove(i);
+		while(cnt<arrLen) {
+			if(arrList.get(idx)==0) {
+				arrList.remove(idx);
 				arrList.add(0);
+			}else {
+				idx++;
 			}
+			cnt++;
 		}
+		for(int i=0;i<arrLen;i++) {
+			System.out.println(arrList.get(i));
+		}
+//		String a ="[";
+//		System.out.println(arrList.size());
+//		while(temp<arrLen) {
+//			if(arr[temp]==0) {
+//				cnt+=1;
+//			}else {
+//				a+=arr[temp]+", ";
+//			}
+//			temp++;
+//		}
+//		for(int i=0; i<cnt-1;i++){
+//			a+="0, ";
+//		}
+//		a+="0]";
+//		System.out.println(a);
 		
 		for (int i = 0; i < arrList.size(); i++) {
 			System.out.print(arrList.get(i));
