b9ecab1f37657a8d38f862b43b3dd63ea512117b
taewan <ntw2100@gmail.com>
2022-05-22 15:47:56 +0900

    백준 2523 - 별찍기 13

--- src/backjoon/practice1/Star_2523.java
+++ src/backjoon/practice1/Star_2523.java
@@ -1,16 +1,15 @@
-package backjoon;
+package backjoon.practice1;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-//import java.util.Scanner;
+
 
 public class Star_2523 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		//첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다. 
 		//첫째 줄부터 2×N-1번째 줄까지 차례대로 별을 출력한다.
-//		Scanner sc = new Scanner(System.in);
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); //선언
 //		String s = bf.readLine(); //String
 //		int i = Integer.parseInt(bf.readLine()); //Int
