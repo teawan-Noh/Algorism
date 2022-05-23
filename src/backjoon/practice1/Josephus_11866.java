45db3e47b71dcbb580b4a90dd7285e2e4765c98a
taewan <ntw2100@gmail.com>
2022-05-24 01:22:09 +0900

    백준11866 - 요세푸스 0 코드 업데이트
    StringTokenizer, StringBuilder 추가

--- src/backjoon/practice1/Josephus_11866.java
+++ src/backjoon/practice1/Josephus_11866.java
@@ -4,37 +4,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.StringTokenizer;
 
-public class Josephus {
+public class Josephus_11866 {
 
 	public static void main(String[] args) throws IOException {
 		//첫째 줄에 N과 K가 빈 칸을 사이에 두고 순서대로 주어진다. (1 ≤ K ≤ N ≤ 1,000) ex)7 3
 		
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); //선언
-//		String s = bf.readLine(); //String
-//		int i = Integer.parseInt(bf.readLine()); //Int
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //선언
+
+		StringTokenizer inputVal = new StringTokenizer(br.readLine());
 		
-		String inputVal = bf.readLine();
-		
-		int cnt = Integer.parseInt(inputVal.split(" ")[0]);
-		int target = Integer.parseInt(inputVal.split(" ")[1]);
+		int cnt = Integer.parseInt(inputVal.nextToken());
+		int target = Integer.parseInt(inputVal.nextToken());
 		
 		ArrayDeque<Integer> arr = new ArrayDeque<Integer>();
 		for (int i = 1; i <= cnt; i++) {
 			arr.addLast(i);
 		}
-
-		String output = "<";
+		
+		StringBuilder sb = new StringBuilder("<");
 		while(!arr.isEmpty()) {
 			for (int i = 0; i < target-1; i++) {
 				int temp = arr.pop();
 				arr.addLast(temp);
 			}
-			output += arr.pop() + ", ";
+			sb.append(arr.pop() + ", ");
 		}
-		int len = output.length();
-		output = output.substring(0, len-2); //마지막 ", " 삭제
-		output += ">";
-		System.out.print(output);
+		int len = sb.length();
+		sb = sb.delete(len-2, len); //마지막 ", " 삭제
+		sb.append(">");
+		System.out.print(sb);
+		
+		br.close();
 	}
 }
